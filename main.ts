3552ae06d849e64be208006af6e81028c03d0c6c
ParminChickenNugget <71943112+ParminChickenNugget@users.noreply.github.com>
2021-02-13 15:30:48 -0500

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,37 @@
+namespace SpriteKind {
+    export const UnCollectedBerry = SpriteKind.create()
+    export const CollectedBerry = SpriteKind.create()
+}
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     snail.y += -16
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     snail.x += -16
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.UnCollectedBerry, function (snail, berry) {
+    if (heldBerry == null) {
+        heldBerry = berry
+        berry.setImage(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `)
+        berry.setKind(SpriteKind.CollectedBerry)
+        berriesLeft += 0 - 1
+    }
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     snail.x += 16
@@ -10,9 +39,71 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     snail.y += 16
 })
+scene.onOverlapTile(SpriteKind.Player, sprites.castle.tileGrass2, function (sprite, location) {
+    if (heldBerry != null) {
+        music.baDing.playUntilDone()
+        tiles.placeOnTile(heldBerry, location)
+        heldBerry.setImage(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `)
+        heldBerry = null
+if (berriesLeft == 0) {
+            game.over(true)
+        }
+    }
+})
+function spawnBerries (numBerries: number, startColumn: number, startRow: number, gap: number) {
+    for (let index = 0; index < numBerries; index++) {
+        berry = sprites.create(img`
+            . . . . . . . 6 . . . . . . . . 
+            . . . . . . 8 6 6 . . . 6 8 . . 
+            . . . e e e 8 8 6 6 . 6 7 8 . . 
+            . . e 2 2 2 2 e 8 6 6 7 6 . . . 
+            . e 2 2 4 4 2 7 7 7 7 7 8 6 . . 
+            . e 2 4 4 2 6 7 7 7 6 7 6 8 8 . 
+            e 2 4 5 2 2 6 7 7 6 2 7 7 6 . . 
+            e 2 4 4 2 2 6 7 6 2 2 6 7 7 6 . 
+            e 2 4 2 2 2 6 6 2 2 2 e 7 7 6 . 
+            e 2 4 2 2 4 2 2 2 4 2 2 e 7 6 . 
+            e 2 4 2 2 2 2 2 2 2 2 2 e c 6 . 
+            e 2 2 2 2 2 2 2 4 e 2 e e c . . 
+            e e 2 e 2 2 4 2 2 e e e c . . . 
+            e e e e 2 e 2 2 e e e c . . . . 
+            e e e 2 e e c e c c c . . . . . 
+            . c c c c c c c . . . . . . . . 
+            `, SpriteKind.UnCollectedBerry)
+        berry.z = -1
+        tiles.placeOnTile(berry, tiles.getTileLocation(startColumn, startRow))
+        startColumn += 1 + gap
+    }
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    otherSprite.destroy()
+})
 let left_car: Sprite = null
 let right_car: Sprite = null
+let startColumn = 0
+let berry: Sprite = null
+let berriesLeft = 0
 let snail: Sprite = null
+let heldBerry: Sprite = null
+info.setLife(5)
 tiles.setTilemap(tilemap`level1`)
 scene.setBackgroundColor(13)
 snail = sprites.create(img`
@@ -33,9 +124,11 @@ snail = sprites.create(img`
     . . . c c 5 5 5 5 c c c c . . . 
     . . . . c c c c c c . . . . . . 
     `, SpriteKind.Player)
-tiles.placeOnTile(snail, tiles.getTileLocation(8, 1))
+tiles.placeOnTile(snail, tiles.getTileLocation(8, 29))
 scene.cameraFollowSprite(snail)
 snail.setStayInScreen(true)
+spawnBerries(4, 5, 1, 1)
+berriesLeft = 4
 game.onUpdateInterval(500, function () {
     right_car = sprites.create(img`
         . . . . . . . . . . . . . . . . 
@@ -56,6 +149,9 @@ game.onUpdateInterval(500, function () {
         . . . . . . . . . . . . . . . . 
         `, SpriteKind.Enemy)
     tiles.placeOnRandomTile(right_car, assets.tile`tile`)
+    right_car.vx = 50
+    right_car.x = -20
+    right_car.setFlag(SpriteFlag.DestroyOnWall, true)
     left_car = sprites.create(img`
         . . . . . . . . . . . . . . . . 
         . . . . . . 2 2 2 2 2 2 2 2 . . 
@@ -75,4 +171,7 @@ game.onUpdateInterval(500, function () {
         . . . . . . . . . . . . . . . . 
         `, SpriteKind.Enemy)
     tiles.placeOnRandomTile(left_car, sprites.vehicle.roadHorizontal)
+    left_car.vx = -50
+    left_car.x = 180
+    left_car.setFlag(SpriteFlag.DestroyOnWall, true)
 })
